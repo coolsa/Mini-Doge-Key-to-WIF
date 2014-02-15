8992df6d80ef5c758166974804e0c87a58a72c5e
coolsa <noob.cloud@gmail.com>
2014-02-14 21:10:38 -0700

    Update btctools.py

--- btctools.py
+++ btctools.py
@@ -7,14 +7,25 @@ digits58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 was contributed by JeromeS at BitcoinTalk.org
 https://bitcointalk.org/index.php?topic=84238.0
 The version used is contributed by ConceptPending at github
-https://github.com/ConceptPending/PythonBTCTools 
+https://github.com/ConceptPending/PythonBTCTools
 """
 
-def minwif(numpriv):
+def pupimp(numpriv):
+    test = hashlib.sha256(numpriv+"?").hexdigest()
+    if test[:2]=="00":
+        step0 = hashlib.sha256(numpriv).hexdigest()
+        step1 = '9e'+step0[0:].strip('L').zfill(64)
+        step2 = hashlib.sha256(binascii.unhexlify(step1)).hexdigest()
+        step3 = hashlib.sha256(binascii.unhexlify(step2)).hexdigest()
+        step4 = int(step1 + step3[:8] , 16)
+        return ''.join([t[step4/(58**l)%58] for l in range(100)])[::-1].lstrip('1')
+    else:
+        return "Invalid Puppykey"
+
+def brain(numpriv):
     step0 = hashlib.sha256(numpriv).hexdigest()
     step1 = '9e'+step0[0:].strip('L').zfill(64)
     step2 = hashlib.sha256(binascii.unhexlify(step1)).hexdigest()
     step3 = hashlib.sha256(binascii.unhexlify(step2)).hexdigest()
     step4 = int(step1 + step3[:8] , 16)
     return ''.join([t[step4/(58**l)%58] for l in range(100)])[::-1].lstrip('1')
-
